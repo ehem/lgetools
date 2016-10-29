d4d0d33e211702aacf1cf290dc8b50cd9d17046d
Elliott Mitchell <ehem+lgetools@m5p.com>
2016-10-29 15:51:01 -0700

    Improvements in GPT finding code
    I like shifting to compute block size, but I concede non-power of 2 sizes
    have shown up in real world devices.  Make it possible to force loadgpt()
    to look for a primary or backup GPT.  Start making example driver
    function actually examine what was returned.  Ensure copyright notice is
    on all files, not just LICENSE.txt.

--- src/mergegpt.c
+++ src/mergegpt.c
@@ -1,5 +1,30 @@
+/* **********************************************************************
+* Copyright (C) 2016 Elliott Mitchell <ehem+android@m5p.com>		*
+*									*
+*	This program is free software: you can redistribute it and/or	*
+*	modify it under the terms of the GNU General Public License as	*
+*	published by the Free Software Foundation, either version 3 of	*
+*	the License, or (at your option) any later version.		*
+*									*
+*	This program is distributed in the hope that it will be useful,	*
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of	*
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*
+*	GNU General Public License for more details.			*
+*									*
+*	You should have received a copy of the GNU General Public	*
+*	License along with this program.  If not, see			*
+*	<http://www.gnu.org/licenses/>.					*
+************************************************************************/
+
+
 /* need tool to merge
 PrimaryGPT_0.bin
 BackupGPT_122141696.bin
 to /dev/block/mmcblk0
 have device as parameter? */
+/*
+-r	Replace GPT
+-m	Merge GPT
+*/
+/* UEFI specification, write backupGPT first, primaryGPT second */
+
