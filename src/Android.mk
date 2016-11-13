691deebfd8a3083849270638058f24693cb490f0
Elliott Mitchell <ehem+lgetools@m5p.com>
2016-11-12 16:13:51 -0800

    Introduce Makefiles for building
    With these in place everything builds with the Android NDK.  There is
    also a build for host machines for the purpose of testing.  Needed to
    bring in a uuid.h since the Android NDK lacks the functionality.  This
    should be usable once built.  The merge strategies are incomplete, still
    need the functionality of fixing things so the CRC remains the same.  The
    intermediate merge strategy also needs to be completed.

---
+++ src/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mergegpt
+
+LOCAL_SRC_FILES := mergegpt.c gpt.c
+
+LOCAL_STATIC_LIBRARIES := android_support
+
+GPTLIBS := z
+
+LOCAL_LDLIBS := $(GPTLIBS:%=-l%)
+
+LOCAL_CFLAGS := -Wall -std=c11
+
+include $(BUILD_EXECUTABLE)
+
+$(call import-module,android/support)
+
---
+++ src/Application.mk
@@ -0,0 +1,4 @@
+APP_PLATFORM := 21
+#APP_ABI := x86 x86_64 armeabi arm64-v8a mips mips64
+APP_ABI := x86 armeabi mips
+
